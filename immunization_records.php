0f26008f1795e776b01948a57e7190d4f7425cae
Abdulwahid-Palanggalan <ojt@sfgroupcompanies.com>
2026-03-04 16:27:07 +0800

    Update immunization_records.php

--- immunization_records.php
+++ immunization_records.php
@@ -87,6 +87,7 @@ $babies = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <title>Immunization Records - Kibenes eBirth</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <?php include_once __DIR__ . '/includes/tailwind_config.php'; ?>
     <style type="text/tailwindcss">
